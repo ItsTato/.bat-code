46f08fcebf33d679e643e29fcaab22bff947e71e
ItsTato <ItsTato@gmail.com>
2021-02-11 19:48:03 -0600

    Made improved version, changes needed in the .bat!

--- always-open-mc-server/always_open_mc_server.bat
+++ always-open-mc-server/always_open_mc_server.bat
@@ -1,10 +1,16 @@
 @echo off
 echo [ItsTatoSoftware] [Info] [%time%] Welcome to the ItsTatoSoftware server console.
 title [ItsTatoSoftware] Server A-1: Running
+CD C:\Program Files (x86)\Google\Chrome\Application\   
+start chrome.exe C:\Users\tato\Desktop\.bat-code\always-open-mc-server\minecraft.html
+CD C:\Users\tato\Desktop\.bat-code\always-open-mc-server
 :SS
 echo [ItsTatoSoftware] [Info] [%time%] Starting minecraft server...
 title [ItsTatoSoftware] Server A-1: Running
 java -Xmx1000M -jar server.jar nogui
+CD C:\Program Files (x86)\Google\Chrome\Application\   
+start chrome.exe C:\Users\tato\Desktop\.bat-code\always-open-mc-server\oops.html
+CD C:\Users\tato\Desktop\.bat-code\always-open-mc-server
 title [ItsTatoSoftware] Server A-1: Crashed, starting
 echo [ItsTatoSoftware] [Crash] [%time%] Server has been reported closed, restarting.
 goto SS
