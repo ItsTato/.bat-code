966640c5c19c11ee6b2fa05490c95592d044c429
ItsTato <ItsTato@gmail.com>
2021-02-21 22:14:41 -0600

    New v1.5.7

--- open-a-program/run_program.bat
+++ open-a-program/run_program.bat
@@ -2,7 +2,6 @@
 title [ItsTatoSoftware]: Are you sure you want to open?
 set /p Run=[ItsTatoSoftware]: Run software? y/n: 
 if %Run%==y (
-	echo [ItsTatoSoftware]: You are being redirected! Continue?
-	pause
+	echo [ItsTatoSoftware]: You are being redirected!
 	EXAMPLE_want_you_gone.wav
 	)
